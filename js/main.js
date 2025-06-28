6a1628ee1dabd32a5f585ddc2dee5ff5e0e339c5
Comfass <comfass.official@gmail.com>
2025-06-28 13:54:06 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
+// js/main.js
+
+import './popup.js';
+import './rating.js';
+import './review.js';
+import './testimonial.js';
+import './theme-music.js';
+import './validation.js';
+import './contact.js';
+import './submit.js';
 
